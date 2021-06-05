3dca1856734d08e72b7ae8a62d1de1a7659103d6
daved01 <david.kirchhoff@icloud.com>
2021-06-05 21:22:34 +0200

    Initial Commit

---
+++ DriverAssist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DriverAssist
+//
+//  Created by David Kirchhoff on 2021-06-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DriverAssist/DriverAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriverAssistApp.swift
+//  DriverAssist
+//
+//  Created by David Kirchhoff on 2021-06-05.
+//
+
+import SwiftUI
+
+@main
+struct DriverAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
